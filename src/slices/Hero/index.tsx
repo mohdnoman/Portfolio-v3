d206bcd5efaf5fc142381308fc123543c6e5fc1c
mohd noman <mohdnoman2751@gmail.com>
2024-02-13 15:59:07 +0530

    added animated objects with sound in Home page.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -2,7 +2,9 @@
 import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import {gsap} from "gsap";
+import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 /**
  * Props for `Hero`.
  */
@@ -60,12 +62,13 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+      <Shapes />
       <div className="col-start-1 md:row-start-1">
         <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name +" "+ slice.primary.last_name}>
     <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first") }</span>
@@ -74,9 +77,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     </h1>
     <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
     </div>
+   
     </div>
 
-    </section>
+    </Bounded>
   );
 };
 
