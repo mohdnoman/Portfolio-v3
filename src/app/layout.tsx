aafe36677859fa0cec1adb3e3e515902b173e6dc
mohd noman <mohdnoman2751@gmail.com>
2024-02-18 18:45:44 +0530

    Added nevigation Bar and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -19,8 +20,9 @@ export default function RootLayout({
     <html lang="en" className="bg-slate-900 text-slate-100">
       
       <body className={urbanist.className}>
-      <Header />
+        <Header />
         {children}
+        <Footer />
         </body>
     </html>
   );
